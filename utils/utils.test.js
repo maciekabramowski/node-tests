35f553b3962ea87c998404ecf31097ea2d591a72
Maciek Abramowski <m.abramowski@wp.pl>
2017-09-18 14:59:42 +0200

    tests with expect

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,15 +1,48 @@
+const expect = require('expect');
+
 const utils = require('./utils');
 
 it('should add two numbers', () => {
   var res = utils.add(33,11);
-  if(res !== 44){
-  throw new Error(`Expected 44, but got ${res}`);
-}
+
+  expect(res).toBe(44).toBeA('number');
+
+//   if(res !== 44){
+//   throw new Error(`Expected 44, but got ${res}`);
+// }
 });
 
 it('should square a number', () => {
   var res = utils.square(3);
-  if(res !== 9){
-  throw new Error(`Expected 9, but got ${res}`);
-}
+
+expect(res).toBe(9).toBeA('number');
+
+//   if(res !== 9){
+//   throw new Error(`Expected 9, but got ${res}`);
+// }
+});
+
+// it('should expect some values', () => {
+//   // expect(12).toNotBe(11);
+//   // expect({name: 'andrew'}).toNotEqual({name: 'Andrew'});
+//   // expect([2,3,4]).toExclude(1);
+//   expect({
+//     name: 'Andrew',
+//     age: 25,
+//     location: 'Philadelphia'
+//   }).toExclude({
+//     age: 23
+//   })
+// });
+
+it('should be the names', () => {
+ var user = {location: 'Philadelphia', age:25};
+ var res = utils.setName(user, 'Andrew Mead');
+
+ // expect(user).toEqual(res);
+
+ expect(res).toInclude({
+   firstName: 'Andrew',
+   lastName: 'Mead'
+ });
 });
