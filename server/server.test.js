7f4ede6ce68f147d499108048a353497279aa66c
Maciek Abramowski <m.abramowski@wp.pl>
2017-09-18 16:20:49 +0200

    organizing tests with describe()

--- server/server.test.js
+++ server/server.test.js
@@ -3,28 +3,34 @@ const expect = require('expect');
 
 var app = require('./server').app;
 
-it('should return hello world response', (done)=>{
-  request(app)
-  .get('/')
-  .expect(404)
-  .expect((res)=>{
-    expect(res.body).toInclude({
-      error: 'Page not found'
-    });
-  })
-  .end(done);
-});
+describe('Server', ()=>{
 
-
-it('should return user Maciek', (done)=>{
-  request(app)
-  .get('/users')
-  .expect(200)
-  .expect((res)=>{
-    expect(res.body).toInclude({
-      name: 'Maciek',
-      age:27
+  describe('GET/',()=>{
+    it('should return hello world response', (done)=>{
+      request(app)
+      .get('/')
+      .expect(404)
+      .expect((res)=>{
+        expect(res.body).toInclude({
+          error: 'Page not found'
+        });
+      })
+      .end(done);
     });
-  })
-  .end(done);
-});
+  });
+
+  describe('GET/users',()=>{
+    it('should return user Maciek', (done)=>{
+      request(app)
+      .get('/users')
+      .expect(200)
+      .expect((res)=>{
+        expect(res.body).toInclude({
+          name: 'Maciek',
+          age:27
+        });
+      })
+      .end(done);
+    });
+  });
+  });
--- utils/utils.test.js
+++ utils/utils.test.js
@@ -2,25 +2,48 @@ const expect = require('expect');
 
 const utils = require('./utils');
 
-it('should add two numbers', () => {
-  var res = utils.add(33,11);
+describe('Utils', ()=>{
 
-  expect(res).toBe(44).toBeA('number');
+  describe('#add', ()=>{
+  it('should add two numbers', () => {
+    var res = utils.add(33,11);
 
-//   if(res !== 44){
-//   throw new Error(`Expected 44, but got ${res}`);
-// }
+    expect(res).toBe(44).toBeA('number');
+
+  //   if(res !== 44){
+  //   throw new Error(`Expected 44, but got ${res}`);
+  // }
+  });
+  });
+
+  it('should async add two numbers', (done) => {
+    utils.asyncAdd(4,3,(sum)=>{
+      expect(sum).toBe(7).toBeA('number');
+      done();
+    });
+  });
+
+
+  it('should square a number', () => {
+    var res = utils.square(3);
+
+  expect(res).toBe(9).toBeA('number');
+
+  //   if(res !== 9){
+  //   throw new Error(`Expected 9, but got ${res}`);
+  // }
+  });
+
+
+  it('should async square number', (done)=>{
+    utils.asyncSquare(3, (square)=>{
+      expect(square).toBe(9).toBeA('number');
+      done();
+    });
+  });
 });
 
-it('should square a number', () => {
-  var res = utils.square(3);
 
-expect(res).toBe(9).toBeA('number');
-
-//   if(res !== 9){
-//   throw new Error(`Expected 9, but got ${res}`);
-// }
-});
 
 // it('should expect some values', () => {
 //   // expect(12).toNotBe(11);
@@ -45,18 +68,4 @@ it('should be the names', () => {
    firstName: 'Andrew',
    lastName: 'Mead'
  });
-});
-
-it('should async add two numbers', (done) => {
-  utils.asyncAdd(4,3,(sum)=>{
-    expect(sum).toBe(7).toBeA('number');
-    done();
-  });
-});
-
-it('should async square number', (done)=>{
-  utils.asyncSquare(3, (square)=>{
-    expect(square).toBe(9).toBeA('number');
-    done();
-  });
 });
