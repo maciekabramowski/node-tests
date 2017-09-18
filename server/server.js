20012f19afbb2bd2725813cdacfe3640ddbe5b32
Maciek Abramowski <m.abramowski@wp.pl>
2017-09-18 16:08:25 +0200

    testing express app

--- server/server.js
+++ server/server.js
@@ -3,7 +3,28 @@ const express = require('express');
 var app = express();
 
 app.get('/', (req,res)=>{
-  res.send('Hello world!');
+  res.status(404).send({
+  error:'Page not found',
+  name: 'Todo App v1.0'
+});
+});
+
+app.get('/users', (req,res)=>{
+  res.status(200).send([
+    {
+      name: 'Olek',
+      age: 28
+    },
+    {
+      name: 'Maciek',
+      age: 27
+    },
+    {
+      name: 'Maria',
+      age: 27
+    }
+  ]);
 });
 
 app.listen(3000);
+module.exports.app = app;
